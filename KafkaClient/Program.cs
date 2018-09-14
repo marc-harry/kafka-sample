913426775e6eda1af35e4b3ba650efa8a8d61ec3
marc.harry <marc.harry@dezrez.com>
2018-09-14 13:17:19 +0100

    Add project files.

---
+++ KafkaClient/Program.cs
@@ -0,0 +1,96 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KafkaClient
+{
+    class Program
+    {
+        private static string[] topics = new string[] { "testTopik" };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var config = new Dictionary<string, object>
+            {
+                { "bootstrap.servers", "127.0.0.1" },
+                { "group.id", "csharp-consumer" },
+                { "enable.auto.commit", false },
+                { "statistics.interval.ms", 60000 },
+                { "session.timeout.ms", 6000 },
+                { "auto.offset.reset", "smallest" }
+            };
+
+            using (var consumer = new Consumer<Ignore, string>(config, null, new StringDeserializer(Encoding.UTF8)))
+            {
+                // Note: All event handlers are called on the main thread.
+
+                consumer.OnPartitionEOF += (_, end)
+                    => Console.WriteLine($"Reached end of topic {end.Topic} partition {end.Partition}, next message will be at offset {end.Offset}");
+
+                consumer.OnError += (_, error)
+                    => Console.WriteLine($"Error: {error}");
+
+                // Raised on deserialization errors or when a consumed message has an error != NoError.
+                consumer.OnConsumeError += (_, error)
+                    => Console.WriteLine($"Consume error: {error}");
+
+                // Raised when the consumer is assigned a new set of partitions.
+                consumer.OnPartitionsAssigned += (_, partitions) =>
+                {
+                    Console.WriteLine($"Assigned partitions: [{string.Join(", ", partitions)}], member id: {consumer.MemberId}");
+                    // If you don't add a handler to the OnPartitionsAssigned event,
+                    // the below .Assign call happens automatically. If you do, you
+                    // must call .Assign explicitly in order for the consumer to 
+                    // start consuming messages.
+                    consumer.Assign(partitions);
+                };
+
+                // Raised when the consumer's current assignment set has been revoked.
+                consumer.OnPartitionsRevoked += (_, partitions) =>
+                {
+                    Console.WriteLine($"Revoked partitions: [{string.Join(", ", partitions)}]");
+                    // If you don't add a handler to the OnPartitionsRevoked event,
+                    // the below .Unassign call happens automatically. If you do, 
+                    // you must call .Unassign explicitly in order for the consumer
+                    // to stop consuming messages from it's previously assigned 
+                    // partitions.
+                    consumer.Unassign();
+                };
+
+                consumer.OnStatistics += (_, json)
+                    => Console.WriteLine($"Statistics: {json}");
+
+                consumer.Subscribe(topics);
+
+                Console.WriteLine($"Started consumer, Ctrl-C to stop consuming");
+
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) =>
+                {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cancelled = true;
+                };
+
+                while (!cancelled)
+                {
+                    if (!consumer.Consume(out Message<Ignore, string> msg, TimeSpan.FromMilliseconds(100)))
+                    {
+                        continue;
+                    }
+
+                    Console.WriteLine($"Topic: {msg.Topic} Partition: {msg.Partition} Offset: {msg.Offset} {msg.Value}");
+
+                    if (msg.Offset % 5 == 0)
+                    {
+                        var committedOffsets = consumer.CommitAsync(msg).Result;
+                        Console.WriteLine($"Committed offset: {committedOffsets}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,86 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Serialization;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace KafkaProducer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string topicName = "testTopik";
+
+            var config = new Dictionary<string, object> { { "bootstrap.servers", "127.0.0.1" } };
+
+            using (var producer = new Producer<string, string>(config, new StringSerializer(Encoding.UTF8), new StringSerializer(Encoding.UTF8)))
+            {
+                Console.WriteLine("\n-----------------------------------------------------------------------");
+                Console.WriteLine($"Producer {producer.Name} producing on topic {topicName}.");
+                Console.WriteLine("-----------------------------------------------------------------------");
+                Console.WriteLine("To create a kafka message with UTF-8 encoded key and value:");
+                Console.WriteLine("> key value<Enter>");
+                Console.WriteLine("To create a kafka message with a null key and UTF-8 encoded value:");
+                Console.WriteLine("> value<enter>");
+                Console.WriteLine("Ctrl-C to quit.\n");
+
+                var cancelled = false;
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true; // prevent the process from terminating.
+                    cancelled = true;
+                };
+
+                while (!cancelled)
+                {
+                    Console.Write("> ");
+
+                    string text;
+                    try
+                    {
+                        text = Console.ReadLine();
+                    }
+                    catch (IOException)
+                    {
+                        // IO exception is thrown when ConsoleCancelEventArgs.Cancel == true.
+                        break;
+                    }
+                    if (text == null)
+                    {
+                        // Console returned null before 
+                        // the CancelKeyPress was treated
+                        break;
+                    }
+
+                    string key = null;
+                    string val = text;
+
+                    // split line if both key and value specified.
+                    int index = text.IndexOf(" ");
+                    if (index != -1)
+                    {
+                        key = text.Substring(0, index);
+                        val = text.Substring(index + 1);
+                    }
+
+                    // Calling .Result on the asynchronous produce request below causes it to
+                    // block until it completes. Generally, you should avoid producing
+                    // synchronously because this has a huge impact on throughput. For this
+                    // interactive console example though, it's what we want.
+                    var deliveryReport = producer.ProduceAsync(topicName, key, val).Result;
+                    Console.WriteLine(
+                        deliveryReport.Error.Code == ErrorCode.NoError
+                            ? $"delivered to: {deliveryReport.TopicPartitionOffset}"
+                            : $"failed to deliver message: {deliveryReport.Error.Reason}"
+                    );
+                }
+
+                // Since we are producing synchronously, at this point there will be no messages
+                // in flight and no delivery reports waiting to be acknowledged, so there is no
+                // need to call producer.Flush before disposing the producer, as you typically 
+                // would.
+            }
+        }
+    }
+}
